7021550ae98c56c32a20eec5023ebafde4bee4b2
Mitul Sharma <misharma@redhat.com>
2024-08-21 15:15:46 +0530

    changes include trimming output,print kdump service status and error log

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"html"
 	"html/template"
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
+	"regexp"
 	"strings"
 
 	"github.com/gorilla/mux"
@@ -17,8 +20,6 @@ type CertificationTest struct {
 	RHCertVersion string   `xml:"rhcert-version,attr"`
 	RHCertRelease string   `xml:"rhcert-release,attr"`
 	Hardware      Hardware `xml:"hardware"`
-	Message       Message  `xml:"message"`
-	Test          Test     `xml:"test"`
 	Output        string   `xml:",innerxml"`
 }
 
@@ -35,21 +36,15 @@ type OS struct {
 	Product string `xml:"product"`
 }
 
-type Message struct {
-	Level string `xml:"level"`
-}
-
-type Test struct {
-	Name string `xml:"name"`
-}
-
-var tmpl = template.Must(template.ParseFiles("upload.html"))
+var tmpl = template.Must(template.ParseFiles(filepath.Join("templates", "index.html")))
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler).Methods("GET")
 	r.HandleFunc("/upload", uploadHandler).Methods("POST")
 	http.Handle("/", r)
+
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	fmt.Println("Starting server on: 8088")
 	http.ListenAndServe(":8088", nil)
@@ -107,37 +102,42 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	output := strings.TrimSpace(certificationTest.Output)
 
-	kdumpConfig := extractSection(output, "kdump configuration:", "updated kdump configuration:")
-	updatedKdumpConfig := extractSection(output, "updated kdump configuration:", "systemctl status kdump:")
-	systemctlStatus := extractSection(output, "systemctl status kdump:", "</message>")
+	kdumpConfig := extractSection(output, "kdump configuration:", "Attempting")
+	updatedKdumpConfig := extractSection(output, "updated kdump configuration:", "restarting kdump with new configuration..")
+	systemctlStatus := extractSection(output, "Checking kdump service", "Crash recovery kernel arming")
+	re := regexp.MustCompile(`Active:\s*(\w+)`)
+	match := re.FindStringSubmatch(systemctlStatus)
+	messageStatus := extractSection(output, `<message level="FAIL">`, "</message>")
 
 	// Debug print
-	fmt.Println("TestName:", certificationTest.Test.Name)
-	fmt.Println("TestStatus:", certificationTest.Message.Level)
 	fmt.Println("KdumpConfig:", kdumpConfig)
 	fmt.Println("UpdatedKdumpConfig:", updatedKdumpConfig)
 	fmt.Println("SystemctlStatus:", systemctlStatus)
+	if len(match) > 1 {
+		fmt.Printf("systemctl status kdump: %s\n", match[1])
+	} else {
+		fmt.Println("Status not found")
+	}
+	fmt.Println("Error Log:", messageStatus)
 
 	data := struct {
 		KernelRelease      string
 		ProductRhel        string
 		RHELVersion        string
 		RhcertVersion      string
-		TestName           string
-		TestStatus         string
 		KdumpConfig        string
 		UpdatedKdumpConfig string
 		SystemctlStatus    string
+		Error              string
 	}{
 		KernelRelease:      certificationTest.Hardware.Release,
 		ProductRhel:        certificationTest.Hardware.OS.Product,
 		RHELVersion:        certificationTest.Hardware.OS.Release,
 		RhcertVersion:      certificationTest.RHCertVersion,
-		TestName:           certificationTest.Test.Name,
-		TestStatus:         certificationTest.Message.Level,
 		KdumpConfig:        kdumpConfig,
 		UpdatedKdumpConfig: updatedKdumpConfig,
-		SystemctlStatus:    systemctlStatus,
+		SystemctlStatus:    match[1],
+		Error:              messageStatus,
 	}
 
 	tmpl.Execute(w, data)
@@ -151,7 +151,10 @@ func extractSection(content, startMarker, endMarker string) string {
 	section := content[startIdx+len(startMarker):]
 	endIdx := strings.Index(section, endMarker)
 	if endIdx == -1 {
-		return section
+		section = strings.TrimSpace(section)
+	} else {
+		section = strings.TrimSpace(section[:endIdx])
 	}
-	return strings.TrimSpace(section[:endIdx])
+
+	return html.UnescapeString(section)
 }
